d3bc47828823f6f1c675c258fda1364e36b889e4
craigdunton <craigodawg19@gmail.com>
2018-04-10 11:12:38 -0400

    Navigation added

---
+++ containers/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { BackHandler } from 'react-native';
+import { NavigationActions, addNavigationHelpers } from 'react-navigation/src/react-navigation';
+import { connect } from 'react-redux';
+import AppNavigator from '../navigator';
+
+@connect(
+  state => ({
+    nav: state.nav,
+  }),
+  dispatch => ({ dispatch }),
+)
+export default class AppWithNavigationState extends Component {
+  static propTypes = {
+    dispatch: PropTypes.func.isRequired,
+    nav: PropTypes.object.isRequired,
+  };
+
+  componentDidMount() {
+    this.backHandler = BackHandler.addEventListener('backPress', () => {
+      console.log("backpress");
+      this.props.dispatch(NavigationActions.back());
+      return true;
+    });
+  }
+
+  componentWillUnmount() {
+    this.backHandler.remove();
+  }
+
+  render() {
+    const { dispatch, nav } = this.props;
+    //<AppNavigator navigation={addNavigationHelpers({ dispatch, state: nav })} />
+    return <AppNavigator />;
+  }
+}
